a7bdeaadf36a4c47abfbbb81ca0a1a22fdafde72
AncorMx <angelcortez05@hotmail.com>
2025-10-26 11:38:40 -0700

    Creación repo

---
+++ src/main/java/com/mycompany/adopcionesmascota/AdopcionesMascota.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adopcionesmascota;
+
+/**
+ *
+ * @author angel
+ */
+public class AdopcionesMascota {
+
+    public static void main(String[] args) {
+        System.out.println("Aasasdasd");
+    }
+}
