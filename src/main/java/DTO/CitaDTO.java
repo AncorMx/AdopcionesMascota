bb7d9c16ffadd3b03e5120dc2021d90a8c7ea6e5
Josel <Josel@Asus>
2025-10-26 19:18:57 -0700

    Clase citadto

---
+++ src/main/java/DTO/CitaDTO.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Josel
+ */
+public class CitaDTO {
+
+    public LocalDateTime fechaHora;
+    public SolicitudAdopcionDTO solicitud;
+    public MascotaDTO mascota;
+    public UsuarioDTO usuario;
+
+    public CitaDTO() {
+    }
+
+    public CitaDTO(LocalDateTime fechaHora, SolicitudAdopcionDTO solicitud, MascotaDTO mascota, UsuarioDTO usuario) {
+        this.fechaHora = fechaHora;
+        this.solicitud = solicitud;
+        this.mascota = mascota;
+        this.usuario = usuario;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public SolicitudAdopcionDTO getSolicitud() {
+        return solicitud;
+    }
+
+    public MascotaDTO getMascota() {
+        return mascota;
+    }
+
+    public UsuarioDTO getUsuario() {
+        return usuario;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public void setSolicitud(SolicitudAdopcionDTO solicitud) {
+        this.solicitud = solicitud;
+    }
+
+    public void setMascota(MascotaDTO mascota) {
+        this.mascota = mascota;
+    }
+
+    public void setUsuario(UsuarioDTO usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "CitaDTO{" + "fechaHora=" + fechaHora + ", solicitud=" + solicitud + ", mascota=" + mascota + ", usuario=" + usuario + '}';
+    }
+
+}
