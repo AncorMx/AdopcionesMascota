0eff4f2648e0cf9522b9679ae7abcc4885729682
Jose Beltran <Josel@Asus>
2025-10-26 21:08:43 -0700

    entitymanager

--- src/main/java/com/mycompany/adopcionesmascota/AdopcionesMascota.java
+++ src/main/java/com/mycompany/adopcionesmascota/AdopcionesMascota.java
@@ -4,6 +4,10 @@
 
 package com.mycompany.adopcionesmascota;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 /**
  *
  * @author angel
@@ -12,5 +16,7 @@ public class AdopcionesMascota {
 
     public static void main(String[] args) {
         System.out.println("Aasasdasd");
+        EntityManagerFactory emf= Persistence.createEntityManagerFactory("AdopcionPU");
+        EntityManager em=emf.createEntityManager();
     }
 }
