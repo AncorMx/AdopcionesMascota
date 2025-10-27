85fe7ef4441b7153c13e98fa0bcf88fe92ba1035
AncorMx <angelcortez05@hotmail.com>
2025-10-26 20:34:04 -0700

    Correcion

--- src/main/java/com/mycompany/adopcionesmascota/GUI/FInfoMascota.java
+++ src/main/java/com/mycompany/adopcionesmascota/GUI/FInfoMascota.java
@@ -2,18 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package GUI;
+package com.mycompany.adopcionesmascota.GUI;
 
 /**
  *
  * @author angel
  */
-public class FInfoVivienda extends javax.swing.JFrame {
+public class FInfoMascota extends javax.swing.JFrame {
 
     /**
-     * Creates new form FInfoVivienda
+     * Creates new form FInfoMascota
      */
-    public FInfoVivienda() {
+    public FInfoMascota() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class FInfoVivienda extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FInfoVivienda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FInfoMascota.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FInfoVivienda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FInfoMascota.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FInfoVivienda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FInfoMascota.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FInfoVivienda.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FInfoMascota.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FInfoVivienda().setVisible(true);
+                new FInfoMascota().setVisible(true);
             }
         });
     }
--- src/main/java/com/mycompany/adopcionesmascota/GUI/FInfoRazones.java
+++ src/main/java/com/mycompany/adopcionesmascota/GUI/FInfoRazones.java
@@ -2,18 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package GUI;
+package com.mycompany.adopcionesmascota.GUI;
 
 /**
  *
  * @author angel
  */
-public class FInfoPersonal extends javax.swing.JFrame {
+public class FInfoRazones extends javax.swing.JFrame {
 
     /**
-     * Creates new form FInfoPersonal
+     * Creates new form FInfoRazones
      */
-    public FInfoPersonal() {
+    public FInfoRazones() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class FInfoPersonal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FInfoPersonal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FInfoRazones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FInfoPersonal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FInfoRazones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FInfoPersonal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FInfoRazones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FInfoPersonal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FInfoRazones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FInfoPersonal().setVisible(true);
+                new FInfoRazones().setVisible(true);
             }
         });
     }
